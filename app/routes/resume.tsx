f1dd96ec5e6c94adf060cdde514a7ad5f603ceb5
Adi <asanand1297@gmail.com>
2025-08-22 17:11:46 +0530

    resume feedback component added

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -72,10 +72,10 @@ const Resume = () => {
           <h2 className="text-4xl !text-black font-bold">Resume Review</h2>
           {feedback ? (
             <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
-              <Summary />
+              <Summary feedback={feedback} />
               <ATS
                 score={feedback.ATS.score || 0}
-                suggestions={feedback.padStart.tips || []}
+                suggestions={feedback.ATS.tips || []}
               />
               <Details feedback={feedback} />
             </div>
